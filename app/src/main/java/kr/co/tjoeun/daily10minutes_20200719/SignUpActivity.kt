dea5594ecaf8fd0532380591ceb45fa17f1666fb
jslee <etoileettoi@gmail.com>
2020-07-19 14:53:10 +0900

    비밀번호 체크로직 구현 + 회원가입 전 미리 데이터 검사 진행

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/SignUpActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/SignUpActivity.kt
@@ -2,7 +2,14 @@ package kr.co.tjoeun.daily10minutes_20200719
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.pwdEdt
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kr.co.tjoeun.daily10minutes_20200719.utils.ServerUtil
 
 class SignUpActivity : BaseActivity() {
 
@@ -14,6 +21,84 @@ class SignUpActivity : BaseActivity() {
     }
 
     override fun setUpEvents() {
+
+//        회원가입 버튼 누르면 => 빈 입력값이 있는지 검사하고
+//        => 괜찮으면 실제로 서버에 가입 요청
+        okBtn.setOnClickListener {
+
+            val inputEmail = emailEdt.text.toString()
+
+            if (inputEmail.isEmpty()) {
+                Toast.makeText(mContext, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+
+//                가입 절차 강제 종료
+                return@setOnClickListener
+            }
+            else if (!inputEmail.contains("@")) {
+//                @가 없다면 이메일 양식 아님
+                Toast.makeText(mContext, "이메일 양식으로 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            이메일 검사는 모두 통과한 상황
+//            비번 길이가 8자 이상인지
+            val inputPw = pwdEdt.text.toString()
+
+            if ( inputPw.length < 8 ) {
+                Toast.makeText(mContext, "비밀번호가 너무 짧습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            닉네임은 입력 했는지만 검사
+             val inputNickName = nickNameEdt.text.toString()
+
+            if(inputNickName.isEmpty()) {
+                Toast.makeText(mContext, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+
+            }
+
+//            이메일 / 비번 / 닉네임 검사를 모두 통과한 상황
+//            서버에 실제로 가입 요청
+//            ServerUtil.
+
+        }
+
+
+//        EditText (비번 입력칸) 에 글자를 타이핑하는 이벤트 체크
+        pwdEdt.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+
+//                비번 확인 로직 실행
+                checkPasswords()
+            }
+
+        })
+    }
+
+//    비밀번호 + 비밀번호 확인 동시에 검사하는 함수
+    fun checkPasswords() {
+
+//        입력한 비밀번호
+        val inputPw = pwdEdt.text.toString()
+
+        if (inputPw.isEmpty() ) {
+            pwCheckResultTxt.text = "비밀번호를 입력해주세요."
+        }
+        else if (inputPw.length < 8) {
+            pwCheckResultTxt.text = "비밀번호가 너무 짧습니다."
+        }
+        else {
+            pwCheckResultTxt.text = "사용해도 좋은 비밀번호 입니다."
+        }
 
     }
 
