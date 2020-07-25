c7d343f87ba6aa2e4938a96e15aa37b7027ab10b
jslee <etoileettoi@gmail.com>
2020-07-25 10:16:51 +0900

    프로젝트 정보 목록에 필요한 데이터클래스 / XML 추가

---
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/data/Project.kt
@@ -0,0 +1,10 @@
+package kr.co.tjoeun.daily10minutes_20200719.data
+
+class Project {
+
+    var id = 0;       //Integer
+    var title = ""    //String
+    var imageUrl = ""
+    var description = ""
+
+}
