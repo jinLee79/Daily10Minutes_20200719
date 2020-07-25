fabcf363700422794fa280a19615561e5712991d
jslee <etoileettoi@gmail.com>
2020-07-25 12:14:45 +0900

    실제 프로젝트 목록 데이터 반영

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ProjectAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ProjectAdapter.kt
@@ -1,12 +1,17 @@
 package kr.co.tjoeun.daily10minutes_20200719.adapters
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10minutes_20200719.R
 import kr.co.tjoeun.daily10minutes_20200719.data.Project
+import org.w3c.dom.Text
 
 class ProjectAdapter(val mContext:Context, resId:Int, val mList:List<Project>) : ArrayAdapter<Project>(mContext, resId, mList) {
 
@@ -19,6 +24,19 @@ class ProjectAdapter(val mContext:Context, resId:Int, val mList:List<Project>) :
         }
 
         val row = tempRow!!
+
+        val projectImg = row.findViewById<ImageView>(R.id.projectImg)
+        val projectTitleViewTxt = row.findViewById<TextView>(R.id.projectTitleViewTxt)
+        val projectDescriptionTxt = row.findViewById<TextView>(R.id.projectDescriptionTxt)
+
+        val data = mList[position]
+
+        projectTitleViewTxt.text = data.title
+        projectDescriptionTxt.text = data.description
+
+//        웹상의 어느 주소에 있는 이미지
+//        어느 화면에서 어떤 데이터를 어디에 뿌려줄거야?
+        Glide.with(mContext).load(data.imageUrl).into(projectImg)
 
         return tempRow
     }
