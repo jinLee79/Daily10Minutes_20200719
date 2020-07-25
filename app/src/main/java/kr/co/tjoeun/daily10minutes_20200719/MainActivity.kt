646a1a06702a048dc91bb3744b09132b566c99f7
jslee <etoileettoi@gmail.com>
2020-07-25 11:37:41 +0900

    JSONArray 파싱 작업 중   - 하나씩 얻어낸 JSONObject를 Project(Class)로 바꿔야 함

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/MainActivity.kt
@@ -35,6 +35,25 @@ class MainActivity : BaseActivity() {
         ServerUtil.getRequestProjectList(mContext, object : ServerUtil.JsonResponseHandler {
             override fun onResponse(json: JSONObject) {
 
+                val data = json.getJSONObject("data")
+                val projects = data.getJSONArray("projects")
+
+//                projects JSONArray 내부의 데이터들을 추출
+//                반복문 i로 돌면서 하나하나 가져오자.
+
+//                for (i in 0..projects.length()-1)
+                for (i in 0 until projects.length()) {
+
+//                    i번째 JSONObject를 추출하자.
+                    val projectObj = projects.getJSONObject(i)
+
+//                    프로젝트 정보 JSONObject => Project 형태의 인스턴스로 변환 => 목록에 담아야 함.
+//                    mProjectList.add(projectObj)  // 잘못된 코딩
+//                    JSON을 Project 변환
+
+
+                }
+
             }
         })
     }
