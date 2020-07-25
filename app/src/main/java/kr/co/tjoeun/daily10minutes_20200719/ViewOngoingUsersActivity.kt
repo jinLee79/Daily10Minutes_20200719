895e77f2c17882dc89d30cf19d6f65f85a0104f0
jslee <etoileettoi@gmail.com>
2020-07-25 14:47:55 +0900

    프로젝트 정보 파싱 => UI 반영  - 명단을 실제로 뿌려줘야 함

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/ViewOngoingUsersActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/ViewOngoingUsersActivity.kt
@@ -3,12 +3,17 @@ package kr.co.tjoeun.daily10minutes_20200719
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_view_ongoing_users.*
+import kr.co.tjoeun.daily10minutes_20200719.data.Project
 import kr.co.tjoeun.daily10minutes_20200719.utils.ServerUtil
 import org.json.JSONObject
 
 class ViewOngoingUsersActivity : BaseActivity() {
 
     var mProjectId = 0
+
+//    서버에서 받아온 프로젝트 정보
+    lateinit var mProject : Project
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +36,17 @@ class ViewOngoingUsersActivity : BaseActivity() {
     fun getOngoingUsersFromServer() {
         ServerUtil.getRequestProjectDetailWithUsers(mContext, mProjectId, object : ServerUtil.JsonResponseHandler{
             override fun onResponse(json: JSONObject) {
-                Log.d("json", json.toString())
+                val data = json.getJSONObject("data")
+                val projectObj = data.getJSONObject("project")
+
+                mProject = Project.getProjectFromJson(projectObj)
+
+//                프로젝트 정보 UI 반영
+                runOnUiThread {
+                    titleTxt.text = mProject.title
+                    userCountTxt.text = "참여 중 인원 : ${mProject.ongoingUsersCount}명"
+                }
+
             }
         })
     }
