03a9d6e9904450512e60c37f7aada17e24869c8b
jslee <etoileettoi@gmail.com>
2020-07-26 14:02:38 +0900

    인증글 내용 반영 중

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ProofAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ProofAdapter.kt
@@ -12,6 +12,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 import kr.co.tjoeun.daily10minutes_20200719.R
 import kr.co.tjoeun.daily10minutes_20200719.data.Proof
 import kr.co.tjoeun.daily10minutes_20200719.data.User
+import java.text.SimpleDateFormat
 
 class ProofAdapter(val mContext:Context, resId:Int, val mList:List<Proof>) : ArrayAdapter<Proof>(mContext, resId, mList) {
 
@@ -26,6 +27,19 @@ class ProofAdapter(val mContext:Context, resId:Int, val mList:List<Proof>) : Arr
 
         val row = tempRow!!
 
+        val userProfileImg = row.findViewById<ImageView>(R.id.userProfileImg)
+        val userNickName = row.findViewById<TextView>(R.id.userNickNameTxt)
+        val proofTime = row.findViewById<TextView>(R.id.proofTimeTxt)
+        val content = row.findViewById<TextView>(R.id.contentTxt)
+        val proofImg = row.findViewById<ImageView>(R.id.proofImg)
+
+        //근거 데이터
+        val data = mList[position]
+
+        content.text = data.content
+        userNickName.text = data.user.nickName
+        proofTime.text = SimpleDateFormat("yyyy년 MM월 dd일 a h시 m분 s초").format(data.proofTime.time)
+        Glide.with(mContext).load(data.user.profileImageList[0].imgUrl).into(userProfileImg)
 
 
         return row
