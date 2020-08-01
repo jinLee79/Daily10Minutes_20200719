09c46999f3611716b289ff60f0b0b802b86f3456
jslee <etoileettoi@gmail.com>
2020-08-01 10:44:52 +0900

    인증 답글 버튼 클릭 시 => 해당 인증 상세화면으로 진입

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/ViewProofDetailActivity.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/ViewProofDetailActivity.kt
@@ -1,8 +1,12 @@
 package kr.co.tjoeun.daily10minutes_20200719
 
 import android.os.Bundle
+import kr.co.tjoeun.daily10minutes_20200719.utils.ServerUtil
 
 class ViewProofDetailActivity : BaseActivity() {
+
+//    목록에서 넘겨주는 인증글의 id값
+    var mProofId = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +20,13 @@ class ViewProofDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mProofId = intent.getIntExtra("proof_id", 0)
+        getProofDataFromServer()
+    }
+
+    fun getProofDataFromServer() {
+//        GET - /project_proof/id값 API 호출
 
     }
 
