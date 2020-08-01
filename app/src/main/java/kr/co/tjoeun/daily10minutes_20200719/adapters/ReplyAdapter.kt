d0041f105fe83176d4bb9cbc835c54481462209b
jslee <etoileettoi@gmail.com>
2020-08-01 16:16:01 +0900

    댓글 좋아요 버튼 누르면 실제 전송

--- app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ReplyAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/daily10minutes_20200719/adapters/ReplyAdapter.kt
@@ -1,22 +1,16 @@
 package kr.co.tjoeun.daily10minutes_20200719.adapters
 
 import android.content.Context
-import android.content.Intent
 import android.os.Handler
 import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import com.bumptech.glide.Glide
 import kr.co.tjoeun.daily10minutes_20200719.R
-import kr.co.tjoeun.daily10minutes_20200719.ViewProofDetailActivity
-import kr.co.tjoeun.daily10minutes_20200719.data.Proof
 import kr.co.tjoeun.daily10minutes_20200719.data.Reply
 import kr.co.tjoeun.daily10minutes_20200719.utils.ServerUtil
-import kr.co.tjoeun.daily10minutes_20200719.utils.TimeUtil
 import org.json.JSONObject
-import java.text.SimpleDateFormat
 
 class ReplyAdapter(val mContext:Context, resId:Int, val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
 
@@ -35,11 +29,39 @@ class ReplyAdapter(val mContext:Context, resId:Int, val mList:List<Reply>) : Arr
         val content = row.findViewById<TextView>(R.id.contentTxt)
         val likeBtn = row.findViewById<Button>(R.id.likeBtn)
 
+        val likeLayout = row.findViewById<LinearLayout>(R.id.likeLayout)
+
         //근거 데이터
         val data = mList[position]
 
         nickName.text = data.writer.nickName
         content.text = data.content
+
+        if (data.likeCount == 0) {
+            likeLayout.visibility = View.GONE
+        }
+
+        likeBtn.setOnClickListener {
+            ServerUtil.postRequestLikeReply(mContext, data.id, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+//                    서버가 주는 메세지를 토스트로 출력
+                    val message = json.getString("message")
+
+                    val uiHandler = Handler(Looper.getMainLooper())
+
+                    uiHandler.post {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+//                        어댑터 새로고침
+                        notifyDataSetChanged()
+                    }
+                }
+
+            })
+        }
+
+
+
 
         return row
     }
